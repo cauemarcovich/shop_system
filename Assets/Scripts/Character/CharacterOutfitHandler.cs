dd7b6f45c8267772603e491b7625594fd3af3894
Caue Marcovich <srmarcovich@gmail.com>
2023-04-30 18:09:43 -0300

    Adding equip feature

--- Assets/Scripts/Character/CharacterOutfitHandler.cs
+++ Assets/Scripts/Character/CharacterOutfitHandler.cs
@@ -1,21 +1,56 @@
 using Scriptable;
+using Scriptable.Core;
 using UnityEngine;
 
 public class CharacterOutfitHandler : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer hair;
-    [SerializeField] private SpriteRenderer body;
-    [SerializeField] private SpriteRenderer pants;
+    [SerializeField] private ItemRuntimeSet equippedItemsVariable;
 
+    [Header("SpriteRenderer References")]
+    [SerializeField] private SpriteRenderer hairSpriteRenderer;
+    [SerializeField] private SpriteRenderer bodySpriteRenderer;
+    [SerializeField] private SpriteRenderer pantsSpriteRenderer;
+
+    private Item _hair;
+    private Item _body;
+    private Item _pants;
+    
     public void Equip(Item item)
     {
-        var spriteRenderer = item.Type switch {
-            ItemType.Hair => hair,
-            ItemType.Body => body,
-            ItemType.Pants => pants,
-            _ => hair
+        switch (item.Type){
+            case ItemType.Hair: EquipHair(item); break;
+            case ItemType.Body: EquipBody(item); break;
+            case ItemType.Pants: EquipPants(item); break;
         };
+    }
 
-        spriteRenderer.sprite = item.Sprite;
+    private void EquipHair(Item newHair)
+    {
+        UpdateEquippedItems(newHair, _hair);
+        _hair = newHair;
+        hairSpriteRenderer.sprite = newHair.Sprite;
+    }
+    
+    private void EquipBody(Item newBody)
+    {
+        UpdateEquippedItems(newBody, _body);
+        _body = newBody;
+        bodySpriteRenderer.sprite = newBody.Sprite;
+    }
+    
+    private void EquipPants(Item newPants)
+    {
+        UpdateEquippedItems(newPants, _pants);
+        _pants = newPants;
+        pantsSpriteRenderer.sprite = newPants.Sprite;
+    }
+
+    private void UpdateEquippedItems(Item newValue, Item oldValue)
+    {
+        if (equippedItemsVariable != null)
+        {
+            equippedItemsVariable.Remove(oldValue);
+            equippedItemsVariable.Add(newValue);
+        }
     }
 }

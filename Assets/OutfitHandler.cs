87e80cfdf732b4f4ed016252bbc3efcfef6b6f30
Caue Marcovich <srmarcovich@gmail.com>
2023-04-30 11:13:04 -0300

    Adding Character oufits

---
+++ Assets/OutfitHandler.cs
@@ -0,0 +1,21 @@
+using Scriptable;
+using UnityEngine;
+
+public class OutfitHandler : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer hair;
+    [SerializeField] private SpriteRenderer body;
+    [SerializeField] private SpriteRenderer pants;
+
+    public void Equip(Item item)
+    {
+        var spriteRenderer = item.Type switch {
+            ItemType.Hair => hair,
+            ItemType.Body => body,
+            ItemType.Pants => pants,
+            _ => hair
+        };
+
+        spriteRenderer.sprite = item.Sprite;
+    }
+}

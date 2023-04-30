751eefaae6a3c3cfd7fc2882011418711ce994c8
Caue Marcovich <srmarcovich@gmail.com>
2023-04-30 10:29:09 -0300

    Creating the inventory

--- Assets/Scripts/Shop/UIShopItemContainer.cs
+++ Assets/Scripts/Shop/UIShopItemContainer.cs
@@ -1,33 +1,46 @@
+using System.Linq;
 using ExtensionMethods;
+using Scriptable;
 using Scriptable.Core;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Shop
 {
     public class UIShopItemContainer : MonoBehaviour
     {
-        [SerializeField] private UIShopItem uiItemContentPrefab;
+        [SerializeField] private UIShopItem uiShopItemPrefab;
         [SerializeField] private ItemRuntimeSet currentItemList;
+        [SerializeField] private ItemRuntimeSet playerInventory;
 
         private void Start()
         {
             Refresh();
         }
 
+        private void OnEnable() => playerInventory.OnValueChanged += Refresh;
+        private void OnDisable() => playerInventory.OnValueChanged -= Refresh;
+
         public void SetItemList(ItemRuntimeSet itemList)
         {
             currentItemList = itemList;
             Refresh();
         }
-        
+
+        public void Refresh(Item item) => Refresh();
+
         public void Refresh()
         {
+            Debug.Log("Refreshing inventory");
+
             transform.DestroyAllChildren();
 
-            for (int i = 0; i < currentItemList.Items.Count; i++)
+            for (int i = 0; i < currentItemList.Items.Count(); i++)
             {
-                var uiShopItem = Instantiate(uiItemContentPrefab, transform);
-                uiShopItem.SetItemData(currentItemList.Items[i]);
+                if (playerInventory.Contains(currentItemList[i])) continue;
+
+                var uiShopItem = Instantiate(uiShopItemPrefab, transform);
+                uiShopItem.SetItemData(currentItemList[i]);
             }
         }
     }

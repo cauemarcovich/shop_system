821c3ebc051211c581b8c7de84ee5e36aba93e71
Caue Marcovich <srmarcovich@gmail.com>
2023-04-30 19:16:19 -0300

    Adding character face direction

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,12 +4,40 @@ namespace Character
 {
     public class CharacterMovement : MonoBehaviour
     {
-        [SerializeField]private float speed;
-        [SerializeField]private Rigidbody2D rb;
-    
+        [SerializeField] private float speed;
+        [SerializeField] private Rigidbody2D rb;
+
+        public MoveDirection _moveDirection;
+
         public void SetMovement(Vector2 movement)
         {
             rb.velocity = movement * speed;
+
+            SetDirection(movement);
         }
+
+        private void SetDirection(Vector2 movement)
+        {
+            if (movement.magnitude < 0.1f) return;
+            
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                if (movement.x > 0f) _moveDirection = MoveDirection.Right;
+                else _moveDirection = MoveDirection.Left;
+            }
+            else
+            {
+                if (movement.y > 0f) _moveDirection = MoveDirection.Up;
+                else _moveDirection = MoveDirection.Down;
+            }
+        }
+    }
+
+    public enum MoveDirection
+    {
+        Up,
+        Right,
+        Down,
+        Left
     }
 }

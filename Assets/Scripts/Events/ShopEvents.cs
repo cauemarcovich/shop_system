d3659cc1ffb981dc417dc01cadc180324bbaa779
Caue Marcovich <srmarcovich@gmail.com>
2023-04-29 23:20:41 -0300

    Changing the UI layout

--- Assets/Scripts/Events/ShopEvents.cs
+++ Assets/Scripts/Events/ShopEvents.cs
@@ -4,5 +4,8 @@ namespace Events
     {
         public const string OPEN_SHOP = "EV_OPEN_SHOP";
         public const string CLOSE_SHOP = "EV_CLOSE_SHOP";
+        
+        public const string BUY_ITEM = "EV_BUY_ITEM";
+        public const string SELL_ITEM = "EV_SELL_ITEM";
     }
 }
--- Assets/Scripts/Shop/UIShopItem.cs
+++ Assets/Scripts/Shop/UIShopItem.cs
@@ -1,21 +1,32 @@
+using Events;
 using Scriptable;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Shop
 {
-    public class UIShopItem : MonoBehaviour
+    public class UIShopItem : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] private Image icon;
         [SerializeField] private TextMeshProUGUI itemName;
         [SerializeField] private TextMeshProUGUI price;
 
+        private Item _item;
+
         public void SetItemData(Item item)
         {
+            _item = item;
+            
             icon.sprite = item.Sprite;
             itemName.text = item.Name;
             price.text = item.Price.ToString();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            EventManager.TriggerEvent(ShopEvents.BUY_ITEM, _item);
         }
     }
 }

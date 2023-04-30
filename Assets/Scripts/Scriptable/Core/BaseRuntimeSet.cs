182f397354c3278e6cb5a5b5d23670fac309ebff
Caue Marcovich <srmarcovich@gmail.com>
2023-04-30 07:50:24 -0300

    Changing the BaseRuntimeSet to have an immutable behaviour (optional)

--- Assets/Scripts/Scriptable/Core/BaseRuntimeSet.cs
+++ Assets/Scripts/Scriptable/Core/BaseRuntimeSet.cs
@@ -5,17 +5,29 @@ namespace Scriptable.Core
 {
     public abstract class BaseRuntimeSet<T> : ScriptableObject
     {
+        [SerializeField] private bool isImmutable;
         [SerializeField] private List<T> items = new();
+        
         public List<T> Items => items;
         
         public void Add(T item)
         {
+            if (isImmutable)
+            {
+                Debug.LogWarning($"An element is been added in the collection '{name}' but it cannot be modified. The operation was ignored.");
+                return;
+            }
             if (!items.Contains(item))
                 items.Add(item);
         }
 
         public void Remove(T item)
         {
+            if (isImmutable)
+            {
+                Debug.LogWarning($"An element is been removed from the collection '{name}' but it cannot be modified. The operation was ignored.");
+                return;
+            }
             if (items.Contains(item))
                 items.Remove(item);
         }

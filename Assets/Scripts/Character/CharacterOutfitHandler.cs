acfed31a0e87a5b74b6b15c57ef6e5391394fe5e
Caue Marcovich <srmarcovich@gmail.com>
2023-05-01 12:05:15 -0300

    Change the character initialization

--- Assets/Scripts/Character/CharacterOutfitHandler.cs
+++ Assets/Scripts/Character/CharacterOutfitHandler.cs
@@ -1,14 +1,20 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using ExtensionMethods;
 using Scriptable;
 using Scriptable.Core;
+using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Character
 {
     public class CharacterOutfitHandler : MonoBehaviour
     {
-        [SerializeField] private ItemRuntimeSet equippedItemsSet;
+        [SerializeField] private Item initialHair;
+        [SerializeField] private Item initialBody;
+        [SerializeField] private Item initialPants;
 
         [Header("SpriteRenderer References")]
         [SerializeField] private SpriteRenderer hairSpriteRenderer;
@@ -16,81 +22,127 @@ namespace Character
         [SerializeField] private SpriteRenderer bodySpriteRenderer;
         [SerializeField] private SpriteRenderer pantsSpriteRenderer;
 
-        private List<Item> _equippedItems = new() { };
+        [Header("Equipped Items Set (Optional)")]
+        [SerializeField] private ItemRuntimeSet equippedItemsSet;
 
-        public Item HairEquipped
+        private Item _hair;
+        private Item _body;
+        private Item _pants;
+
+        // public Item HairEquipped => _hair;
+        // public Item BodyEquipped => _body;
+        // public Item PantsEquipped => _pants;
+
+        private void Start()
         {
-            get
-            {
-                _equippedItems.TryGetFirstByItemType(ItemType.Hair, out Item equip);
-                return equip;
-            }
-        }
-        public Item BodyEquipped
-        {
-            get
-            {
-                _equippedItems.TryGetFirstByItemType(ItemType.Body, out Item equip);
-                return equip;
-            }
-        }
-        public Item PantsEquipped
-        {
-            get
-            {
-                _equippedItems.TryGetFirstByItemType(ItemType.Pants, out Item equip);
-                return equip;
-            }
+            _hair = initialHair;
+            _body = initialBody;
+            _pants = initialPants;
+            UpdateSprites();
         }
 
         public void Equip(Item item)
         {
-            Debug.Log("Euipping");
-            if (_equippedItems.TryGetFirstByItemType(item.Type, out Item equippedItem))
-            {
-                Unequip(equippedItem);
-            }
-
-            _equippedItems.Add(item);
-            if (equippedItemsSet != null)
-            {
-                equippedItemsSet.Add(item);
-            }
-
+            Unequip(item.Type);
+            CacheItem(item);
             UpdateSprites();
         }
 
         public void Unequip(Item item)
         {
-            _equippedItems.Remove(item);
-            if (equippedItemsSet != null)
-            {
-                equippedItemsSet.Remove(item);
-            }
-
+            ClearCache(item);
             UpdateSprites();
+        }
+
+        public void Unequip(ItemType itemType)
+        {
+            var item = itemType switch
+            {
+                ItemType.Hair => _hair,
+                ItemType.Body => _body,
+                ItemType.Pants => _pants,
+                _ => null
+            };
+
+            if (item != null)
+                Unequip(item);
         }
 
         public void UnequipAll()
         {
-            for (int i = 0; i < _equippedItems.Count; i++)
+            if (_hair) ClearCache(_hair);
+            if (_body) ClearCache(_body);
+            if (_pants) ClearCache(_pants);
+            UpdateSprites();
+        }
+
+        private void CacheItem(Item item)
+        {
+            if (item.Type == ItemType.Hair) _hair = item;
+            if (item.Type == ItemType.Body) _body = item;
+            if (item.Type == ItemType.Pants) _pants = item;
+
+            if (equippedItemsSet != null)
             {
-                Unequip(_equippedItems[i]);
+                equippedItemsSet.Add(item);
             }
         }
 
-        public bool HasAnyEquip() => _equippedItems.Count > 0;
+        private void ClearCache(Item item)
+        {
+            if (item.Type == ItemType.Hair) _hair = null;
+            if (item.Type == ItemType.Body) _body = null;
+            if (item.Type == ItemType.Pants) _pants = null;
+
+            if (equippedItemsSet != null)
+            {
+                equippedItemsSet.Remove(item);
+            }
+        }
 
         private void UpdateSprites()
         {
-            _equippedItems.TryGetFirstByItemType(ItemType.Hair, out Item hairEquipped);
-            hairSpriteRenderer.sprite = hairEquipped != null ? hairEquipped.Sprite : null;
+            hairSpriteRenderer.sprite = _hair != null ? _hair.Sprite : null;
+            bodySpriteRenderer.sprite = _body != null ? _body.Sprite : null;
+            pantsSpriteRenderer.sprite = _pants != null ? _pants.Sprite : null;
+        }
 
-            _equippedItems.TryGetFirstByItemType(ItemType.Body, out Item bodyEquipped);
-            bodySpriteRenderer.sprite = bodyEquipped != null ? bodyEquipped.Sprite : null;
+        private ItemRuntimeSet _shopItemsSets;
 
-            _equippedItems.TryGetFirstByItemType(ItemType.Pants, out Item pantsEquipped);
-            pantsSpriteRenderer.sprite = pantsEquipped != null ? pantsEquipped.Sprite : null;
+        [ContextMenu("Load Random Equips")]
+        void LoadRandomEquipsContextMenu()
+        {
+            if (_shopItemsSets == null)
+            {
+                var shopItemsGuid = AssetDatabase.FindAssets("ShopItems")[0];
+                var shopItemsPath = AssetDatabase.GUIDToAssetPath(shopItemsGuid);
+                _shopItemsSets = AssetDatabase.LoadAssetAtPath<ItemRuntimeSet>(shopItemsPath);
+            }
+
+            UnequipAll();
+
+            Item[] items;
+
+            items = _shopItemsSets.Items.GetAllByItemType(ItemType.Hair).ToArray();
+            ChooseRandomAndEquip(items);
+            initialHair = _hair;
+
+            items = _shopItemsSets.Items.GetAllByItemType(ItemType.Body).ToArray();
+            ChooseRandomAndEquip(items);
+            initialBody = _body;
+
+            items = _shopItemsSets.Items.GetAllByItemType(ItemType.Pants).ToArray();
+            ChooseRandomAndEquip(items);
+            initialPants = _pants;
+        }
+
+        void ChooseRandomAndEquip(Item[] items)
+        {
+            if (items.Length > 0)
+            {
+                var item = items[Random.Range(0, items.Count())];
+                Equip(item);
+            }
         }
     }
 }
